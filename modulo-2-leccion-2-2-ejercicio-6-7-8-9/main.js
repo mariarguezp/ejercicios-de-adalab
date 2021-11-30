8a006206303c9402e7e890d249e251ff6d72dcac
mariarguezp <mariarguezp5@gmail.com>
2021-11-30 19:39:16 +0100

    add lesson 2.2 exercises

---
+++ modulo-2-leccion-2-2-ejercicio-6-7-8-9/main.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// ------ EJERCICIO 6 ------
+
+// const name = 'Paula';
+
+// const greetingText = document.querySelector('.text');
+// greetingText.innerHTML += 'Hola ' + name + ', encantada de conocerte.';
+
+
+
+// ------ EJERCICIO 7 ------
+
+// const name = 'Paula';
+
+// const greetingText = document.querySelector('.text');
+// greetingText.innerHTML += `Hola ${name}, encantada de conocerte.`;
+
+
+
+// ------ EJERCICIO 8 ------
+
+// const firstDogImage = 'https://images.dog.ceo/breeds/schipperke/n02104365_8156.jpg';
+// const firstDogName = 'Dina';
+
+// const secondDogImage = 'https://images.dog.ceo/breeds/collie-border/n02106166_355.jpg';
+// const secondDogName = 'Luna';
+
+// const thirdDogImage = 'https://images.dog.ceo/breeds/affenpinscher/n02110627_7065.jpg';
+// const thirdDogName = 'Lana';
+
+// const listElement = document.querySelector('.list');
+// listElement.innerHTML += `<li>
+// <p>${firstDogName}</p>
+// <img src=${firstDogImage}>
+// </li><li>
+// <p>${secondDogName}</p>
+// <img src=${secondDogImage}>
+// </li><li>
+// <p>${thirdDogName}</p>
+// <img src=${thirdDogImage}>
+// </li>`;
+
+
+
+
+// ------ EJERCICIO 9 ------
+
+// const name = 'Paula de la Victoria';
+
+// const textElement = document.querySelector('.text');
+// textElement.innerHTML += `El nombre de mi compañera es ${name}, y está compuesto por ${name.length} caracteres`;
+
